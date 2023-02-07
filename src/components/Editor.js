21bc0e9fc64887a0215d2c1de907c4e5a11a3ec8
dasurahul <dasurahul598@gmail.com>
2023-02-07 17:20:57 +0530

    add options to MonacoEditor component to resize automatic

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -19,6 +19,7 @@ export default function Editor(props) {
         onChange={(value) => setValue(value)}
         value={value}
         theme="vs-dark"
+        options={{ automaticLayout: true }}
       />
     </div>
   );
