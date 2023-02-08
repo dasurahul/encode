c347a907d436ec7a38808a34866cfed83511d27d
dasurahul <dasurahul598@gmail.com>
2023-02-08 17:55:53 +0530

    replace react-monaco-editor with react-ace and ace-builds

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,10 +1,16 @@
-import MonacoEditor from "react-monaco-editor";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/mode-javascript";
+
+import "ace-builds/src-noconflict/theme-twilight";
 
 export default function Editor(props) {
   const { language, value, setValue } = props;
   return (
-    <div>
-      <span className="bg-[#1E1E1E] h-8 p-1">
+    <div className="overflow-hidden">
+      <span className="h-8 p-1">
         {language === "html"
           ? "HTML"
           : language === "css"
@@ -13,13 +19,13 @@ export default function Editor(props) {
           ? "JS"
           : language}
       </span>
-      <MonacoEditor
-        height={300}
-        language={language}
+      <AceEditor
+        name={`ace-editor-${language}`}
+        mode={language}
+        theme="twilight"
+        height="300px"
         onChange={(value) => setValue(value)}
         value={value}
-        theme="vs-dark"
-        options={{ automaticLayout: true }}
       />
     </div>
   );
